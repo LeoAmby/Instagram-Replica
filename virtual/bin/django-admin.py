471d37074ae4ae323dad59b9806cfd5a9b9862fc
Leo <leoamby@gmail.com>
2019-10-13 22:45:43 +0300

    install django==2.0

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/moringa/Documents/MoringaSchool-Projects/Core-Projects/Django/Instagram-Replica/virtual/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
