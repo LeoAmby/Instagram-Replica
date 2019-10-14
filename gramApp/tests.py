cf2bb79e2460743db9ffacec5af3f3867500fafc
Leo <leoamby@gmail.com>
2019-10-14 11:51:26 +0300

    create a test for the profile model

--- gramApp/tests.py
+++ gramApp/tests.py
@@ -14,6 +14,25 @@ class ImageTest(TestCase):
         self.photo.save_image()
         self.assertTrue(Image.name)
 
-    def test_delete():
-        self.image.delete_image()
-        self.assertTrue(Image.name)
+    def delete_image(self):
+        Image.image_name.remove(self)
+
+
+class test_profile(TestCase):
+    def setUp(self):
+        self.photo = Profile(id = 'profilePhoto')
+        self.bio = Profile(bio = 'bio')
+    
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.photo,Profile))
+        self.assertTrue(isinstance(self.bio,Profile))
+
+
+    def test_save(self):
+        self.photo.save_image()
+        self.assertTrue(Profile.name)
+
+    def delete_profile(self):
+        Profile.profile_profpic.remove(self)
+        Profile.profile_bio.remove(self)
