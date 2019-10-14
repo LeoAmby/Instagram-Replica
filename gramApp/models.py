cb51153f80b9e6e0f609301c4a7ec185d051cf64
Leo <leoamby@gmail.com>
2019-10-14 10:14:51 +0300

    rectify the fields in the models

--- gramApp/models.py
+++ gramApp/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 
 class Profile(models.Model):
-    profPic = models.Image(uplade_to = 'images/', blank = True, null=True)
-    bio = models.Textfield()
+    profPic = models.ImageField(upload_to = 'images/', blank = True, null=True)
+    bio = models.TextField()
 
     def __str__(self):
         return self.name
 
-class Image(models.Models):
+class Image(models.Model):
     photo = models.ImageField(upload_to = 'images/', blank = True, null=True  )
-    name = models.CharField(max_len = 30)
-    caption = models.CharField(max_len = 30)
-    profile = models.ForeignKey(profile)
+    name = models.CharField(max_length = 30)
+    caption = models.CharField(max_length = 30)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE,)
     comments = models.TextField()
 
     def __str__ (self):
