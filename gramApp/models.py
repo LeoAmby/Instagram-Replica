390724b0a63c62235cc9d366ddde8598a384b2ae
Leo <leoamby@gmail.com>
2019-10-14 09:59:43 +0300

    creat the profile model and edit the image model

--- gramApp/models.py
+++ gramApp/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
+class Profile(models.Model):
+    profPic = models.Image(uplade_to = 'images/', blank = True, null=True)
+    bio = models.Textfield()
+
+    def __str__(self):
+        return self.name
+
 class Image(models.Models):
     photo = models.ImageField(upload_to = 'images/', blank = True, null=True  )
     name = models.CharField(max_len = 30)
     caption = models.CharField(max_len = 30)
     profile = models.ForeignKey(profile)
     comments = models.TextField()
+
+    def __str__ (self):
+        return self.profpic
