8259dadae67459f9a54178cf1ac09a2bacf603fb
Leo <leoamby@gmail.com>
2019-10-14 09:54:58 +0300

    add a logo image to the static-images folder and edit the Image model

--- gramApp/models.py
+++ gramApp/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Image(models.Models):
-    photo = models.ImageField(upload_to )
+    photo = models.ImageField(upload_to = 'images/', blank = True, null=True  )
     name = models.CharField(max_len = 30)
     caption = models.CharField(max_len = 30)
     profile = models.ForeignKey(profile)
-    comments = models.TextField
+    comments = models.TextField()
