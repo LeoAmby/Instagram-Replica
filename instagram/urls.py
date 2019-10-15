a4763647d27cc990845303f960fb84f7d2b48fd4
Leo <leoamby@gmail.com>
2019-10-15 11:18:58 +0300

    create  a superuser

--- instagram/urls.py
+++ instagram/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, register_converter
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('gramApp.urls')),
+    # path('accounts/', include(registration.backends.simple.urls)),
 ]
