23d0f7249639958c230ebdd341bf9955d7ea89b4
Leo <leoamby@gmail.com>
2019-10-16 10:13:14 +0300

    update email confirmation settings

--- gramApp/forms.py
+++ gramApp/forms.py
@@ -1,5 +1,10 @@
 from django import forms
 
+class signupForm(forms.Form):
+    fullname = forms.CharField
+    username = forms.CharField()
+    email = forms.EmailField()
 
 class loginForm(forms.Forms):
-    
+    username = forms.CharField()
+    password = forms.CharField()
--- instagram/settings.py
+++ instagram/settings.py
@@ -131,3 +131,6 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
