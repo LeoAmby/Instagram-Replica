e9089a288c80a8132d70a18211be1c0e1da0139a
Leo <leoamby@gmail.com>
2019-10-16 10:37:44 +0300

    create a .env file with all the email requirements

--- gramApp/forms.py
+++ gramApp/forms.py
@@ -1,9 +1,13 @@
 from django import forms
+# from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class signupForm(forms.Form):
-    fullname = forms.CharField
-    username = forms.CharField()
-    email = forms.EmailField()
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'fullname', 'email', 'password1', 'password2')
 
 class loginForm(forms.Forms):
     username = forms.CharField()
