e9baa5890e6cdff3ceb99dae769a00b10edd681d
Leo <leoamby@gmail.com>
2019-10-19 09:07:19 +0300

    correct heroku settings

--- instagram/settings.py
+++ instagram/settings.py
@@ -127,7 +127,6 @@ STATICFILES_DIRS = [
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
