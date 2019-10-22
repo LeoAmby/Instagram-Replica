39b796e14587adc3f857482de560528761e5bad3
Leo <leoamby@gmail.com>
2019-10-22 14:18:00 +0300

    deployment

--- instagram/settings.py
+++ instagram/settings.py
@@ -1,12 +1,36 @@
 import os
 import django_heroku
 import dj_database_url
-from decouple import config, Csv
-from dotenv import load_dotenv  
-  
-load_dotenv()
+from decouple import config,Csv
 
+MODE=config("MODE", default="dev")
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+# development
+if config('MODE')=="dev":
+   DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
+           'HOST': config('DB_HOST'),
+           'PORT': '',
+       }
+       
+   }
+# production
+else:
+   DATABASES = {
+       'default': dj_database_url.config(
+           default=config('DATABASE_URL')
+       )
+   }
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
     
 # Build paths inide the project like this: os.path.join(BASE_DIR, ...)
@@ -68,39 +92,39 @@ TEMPLATES = [
     },
 ]
 
-# WSGI_APPLICATION = 'instagram.wsgi.application'
+WSGI_APPLICATION = 'instagram.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-MODE=config("MODE", default="dev")
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
-# development
-if config("MODE")=="dev":
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',  
-            'NAME': config('DB_NAME'),  
-            'USER': config('DB_USER'),  
-            'PASSWORD': config('DB_PASSWORD'),  
-            'HOST': config('DB_HOST'),
-            'PORT': '',
-    }
-}
+# MODE=config("MODE", default="dev")
+# SECRET_KEY = config('SECRET_KEY')
+# DEBUG = config('DEBUG', default=False, cast=bool)
+# # development
+# if config("MODE")=="dev":
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2',  
+#             'NAME': config('DB_NAME'),  
+#             'USER': config('DB_USER'),  
+#             'PASSWORD': config('DB_PASSWORD'),  
+#             'HOST': config('DB_HOST'),
+#             'PORT': '',
+#     }
+# }
 
-else:
-    DATABASES = {
-        'default':dj_database_url.config(
-            default=config('DATABASE_URL')) 
+# else:
+#     DATABASES = {
+#         'default':dj_database_url.config(
+#             default=config('DATABASE_URL')) 
 
-    } 
+#     } 
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
