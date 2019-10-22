9fcdc92515f174bf7173b61adc5551102f95d860
Leo <leoamby@gmail.com>
2019-10-22 14:27:50 +0300

    |deployment

--- instagram/settings.py
+++ instagram/settings.py
@@ -46,6 +46,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Application definition
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '5y=(!7hcwto1ciqv94e&#wf^wm2o2qe$4t3@4y$!lxba21c2b+'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'bootstrap4',
@@ -94,37 +101,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'instagram.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-# MODE=config("MODE", default="dev")
-# SECRET_KEY = config('SECRET_KEY')
-# DEBUG = config('DEBUG', default=False, cast=bool)
-# # development
-# if config("MODE")=="dev":
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',  
-#             'NAME': config('DB_NAME'),  
-#             'USER': config('DB_USER'),  
-#             'PASSWORD': config('DB_PASSWORD'),  
-#             'HOST': config('DB_HOST'),
-#             'PORT': '',
-#     }
-# }
-
-# else:
-#     DATABASES = {
-#         'default':dj_database_url.config(
-#             default=config('DATABASE_URL')) 
-
-#     } 
-
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
-
-# ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
